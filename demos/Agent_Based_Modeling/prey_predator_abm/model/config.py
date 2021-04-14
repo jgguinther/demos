3c5b2d39297be8d45babba9753fa9deb20b0764d
Tyler Mace <tylerdmace@gmail.com>
2021-04-14 10:37:49 -0600

    Import fixes

--- demos/Agent_Based_Modeling/prey_predator_abm/model/config.py
+++ demos/Agent_Based_Modeling/prey_predator_abm/model/config.py
@@ -15,6 +15,7 @@ sim_config = config_sim (
 exp = Experiment()
 
 exp.append_configs(
+    user_id='user_a',
     model_id='sys_model',
     sim_configs=sim_config,
     initial_state=genesis_states,
