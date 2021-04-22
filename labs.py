5939aee0f47c0802d7d3ca53165d1d232b5df9e1
JEJodesty <joshua.jodesty@gmail.com>
2021-04-22 15:49:49 -0400

    converted Predator_Prey_SD_model

--- labs.py
+++ labs.py
@@ -1,6 +1,4 @@
-from pprint import pprint
-
-from demos.System_Dynamics.prey_predator_sd.model import config
-from demos.System_Dynamics.prey_predator_sd.model.config import exp
+from demos.Agent_Based_Modeling.prey_predator_abm.model import config
+from demos.Agent_Based_Modeling.prey_predator_abm.model.config import exp
 
 client_modules = [config]
