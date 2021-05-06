7dfb04f8aa8007dd0ee422820270f97383fc385f
JEJodesty <joshua.jodesty@gmail.com>
2021-05-06 15:50:52 -0400

    Experiment only labs.py

--- labs.py
+++ labs.py
@@ -1,6 +1,9 @@
-from cadCAD.configuration.utils import prep_modules
+# from cadCAD.configuration.utils import prep_modules
 from demos.Agent_Based_Modeling.prey_predator_abm.model.config import exp
 
+# client_modules = prep_modules('/tmp/demos/demos')
+# client_modules = prep_modules('/tmp/demos')
+
 # client_modules = prep_modules('demos/demos')
-client_modules = prep_modules('./demos')
+# client_modules = prep_modules('./demos')
 # client_modules = prep_modules('demos/Agent_Based_Modeling')
