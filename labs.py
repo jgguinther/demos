1db00458120f626508256ec1f0cd57a14941acc0
JEJodesty <joshua.jodesty@gmail.com>
2021-05-09 15:19:22 -0400

    labs.py pre change

--- labs.py
+++ labs.py
@@ -2,5 +2,6 @@ from cadCAD.configuration.utils import prep_modules
 from demos.Agent_Based_Modeling.prey_predator_abm.model.config import exp
 
 # client_modules = prep_modules('demos/demos')
-client_modules = prep_modules('./demos')
+client_modules = prep_modules('demos')
+# client_modules = prep_modules('./demos')
 # client_modules = prep_modules('demos/Agent_Based_Modeling')
