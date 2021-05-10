0ad4d7a41b498800c3607d519dc3c7a057c4adec
JEJodesty <joshua.jodesty@gmail.com>
2021-05-10 10:47:46 -0400

    include setup.py for automated packaging

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
-import os
-from setuptools import setup, find_packages
 from labs import model_dir
-
+from setuptools import setup, find_packages
 
 # Comment Out: in future Users will have created requirements.txt off platform
+# import os
+# os.system("pip3 freeze | grep -v 'pkg-resources' | grep -v 'cadCAD' > requirements.txt")
 # os.system("pip3 freeze > requirements.txt")
 
 with open('requirements.txt') as f:
-    install_requires = f.read().strip().split('\n')
+    install_requires = [
+        pkg for pkg in f.read().strip().split('\n')
+            if not pkg.startswith('pkg-resources') and
+               not pkg.startswith('cadCAD')
+    ]
 
 setup(
-    name="simpkg",
-    version='0.0.1',
-    install_requires=install_requires,
+    name=model_dir,
+    version='0.0.0',
     package_dir={"": model_dir},
     packages=find_packages(where=model_dir),
+    install_requires=install_requires,
     python_requires='>=3.6.8'
 )
-
 # Step 1 - this file (setup.py) will be added to project root
-# Step 2 - run the fllowing command: `python3 setup.py bdist_wheel`
+# Step 2 - run the following command: `python3 setup.py bdist_wheel`
 # Step 3 - send dist/*.whl to S3 bucket
 # Step 4 - send .whl url along with job request
 # Step 5 - endpoint GET .whl with url and installs it
