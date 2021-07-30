bb9e410eb0f39e84a7c572e788a08b62ca7d7e28
Jacob Guinther <jacob@block.science>
2021-07-30 12:57:44 -0600

    Update config.py

--- demos/System_Dynamics/Compartment_Models/seird/config.py
+++ demos/System_Dynamics/Compartment_Models/seird/config.py
@@ -19,7 +19,14 @@ sim_config = config_sim (
 exp = Experiment()
 
 exp.append_model(
-    model_id="sys_modelllllll",
+    model_id="sys_model",
+    sim_configs=sim_config,
+    initial_state=genesis_states,
+    partial_state_update_blocks=partial_state_update_block
+)
+
+exp.append_model(
+    model_id="sys_model",
     sim_configs=sim_config,
     initial_state=genesis_states,
     partial_state_update_blocks=partial_state_update_block
