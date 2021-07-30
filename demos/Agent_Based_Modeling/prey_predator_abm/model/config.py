18442cb7d708ce514dce9e5bb468cecf44c6267e
Jacob Guinther <jacob@block.science>
2021-07-30 12:07:05 -0600

    Update config.py

--- demos/Agent_Based_Modeling/prey_predator_abm/model/config.py
+++ demos/Agent_Based_Modeling/prey_predator_abm/model/config.py
@@ -15,7 +15,7 @@ sim_config = config_sim (
 
 exp = Experiment()
 exp.append_model(
-    model_id='sys_modelssssss',
+    model_id='sys_model',
     sim_configs=sim_config,
     initial_state=genesis_states,
     partial_state_update_blocks=partial_state_update_block
