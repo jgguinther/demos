74e5f6745269f904b2bd2418a12b45d616161e39
Jacob Guinther <jacobgguinther@gmail.com>
2021-08-04 07:51:41 -0600

    Update labs.py

--- labs.py
+++ labs.py
@@ -1,5 +1,5 @@
 # from demos.Agent_Based_Modeling.prey_predator_abm.model.config import exp
 # from demos.System_Dynamics.Compartment_Models.seir.config import exp
-# from demos.System_Dynamics.Compartment_Models.seird.config import exp
+from demos.System_Dynamics.Compartment_Models.seird.config import exp
 
-# model_dir = 'demos'
+model_dir = 'demos'
